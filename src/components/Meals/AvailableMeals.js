5e2c4e9dd6fea1cbb73aa59da3638ed9ee91ba9a
mohakbudhwani <mohakbudhwani1999>
2024-09-07 14:40:46 +0530

    adding Meal Item Form

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -31,7 +31,12 @@ const DUMMY_MEALS = [
 
   const AvailableMeals=()=>{
     const mealList=DUMMY_MEALS.map((meal)=>
-    <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}/>
+    <MealItem 
+    key={meal.id} 
+    name={meal.name} 
+    description={meal.description} 
+    price={meal.price}
+    />
     )
 
     return <section className={classes.meals}>
